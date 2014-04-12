0fd059ba65def7bb00f0baafcc4941bd302dbe12
jamie7132 <jfsmills@umich.edu>
2014-04-12 02:02:26 -0400

    before table changes

--- history.js
+++ history.js
@@ -2,14 +2,12 @@ $(document).ready(function(){
 
 	Parse.initialize("KNf3x2GGrkFOoRapY8D9y6PkrHKRPlk6FgeWblEF", "hfutDYnw41mayLiVZdcNBL3e3d1fB9DAfoAw5XtO");
 
-	console.log("this is working");
+	console.log("history is working");
 	var leader = Parse.Object.extend("Client");
 	var query = new Parse.Query(leader);
-	query.descending("score")
-	console.log("here");
 	query.find({
 		success:function(leaders){
-			//DISPLAY LEADERS]
+			//DISPLAY HISTORY
 			console.log(leaders.length);
 			for(var i = 0; i < leaders.length; i++){
 				console.log(leaders[i].get("ip"));
