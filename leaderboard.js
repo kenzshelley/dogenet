fcd79a32a74b67ef9c4ae3678934ceb1de01f782
jamie7132 <jfsmills@umich.edu>
2014-04-12 02:33:28 -0400

    historyboard working

--- leaderboard.js
+++ leaderboard.js
@@ -2,18 +2,18 @@ $(document).ready(function(){
 
 	Parse.initialize("KNf3x2GGrkFOoRapY8D9y6PkrHKRPlk6FgeWblEF", "hfutDYnw41mayLiVZdcNBL3e3d1fB9DAfoAw5XtO");
 
-	console.log("this is working");
+	console.log("leader is working");
 	var leader = Parse.Object.extend("Client");
 	var query = new Parse.Query(leader);
 	query.descending("score")
-	console.log("here");
+	// console.log("here");
 	query.find({
 		success:function(leaders){
 			//DISPLAY LEADERS]
-			console.log(leaders.length);
+			// console.log(leaders.length);
 			for(var i = 0; i < leaders.length; i++){
-				console.log(leaders[i].get("ip"));
-				console.log(leaders[i].get("score"));
+				// console.log(leaders[i].get("ip"));
+				// console.log(leaders[i].get("score"));
 				//$('#leaderboard').append('<tr><td>leaders[i].get("ip")</td><td>leaders[i].get("score")</td></tr>')
 				var table = document.getElementById("leaderboard-table")
 				var row = table.insertRow(-1);
