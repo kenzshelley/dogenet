bf0a7a9486d0cf77b2087642a6c77a55ccb68f53
Matthew Grossman <matt@mrgrossman.com>
2014-04-12 08:05:08 -0400

    got overriding the shady links to RR

--- linkreplace.py
+++ linkreplace.py
@@ -36,11 +36,20 @@ def redditPhonyArticle(document):
     insertionIndex = random.randint(1,10)
     if (insertionIndex%2 == 1):
         insertionIndex = insertionIndex + 1
-    article.find("a")['href'] = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-    article.find_all("div")[6].find("p").find("a").replace_with('I am a nigerian prince with lots of money, AMA!')
+    newArticle = BeautifulSoup(str(article))
+    newArticle.find("a")['href'] = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    articleDivs = newArticle.find_all("div")
+    for div in articleDivs:
+        if "entry" in div.get("class"):
+            textArea = div
+            break
+
+    textArea.find("p").find("a").append("I am a nigerian prince with lots of money, AMA!")
+    textArea.find("p").find("a")['href'] = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    textArea.find("p").find("a").contents[0] = u''
     siteTable = soup.find("div", {"id":"siteTable"})
     siteTable.insert(insertionIndex, BeautifulSoup(str(divider)))
-    siteTable.insert(insertionIndex + 1, BeautifulSoup(str(article)))
+    siteTable.insert(insertionIndex + 1, newArticle)
 
     return soup.prettify()
 if __name__ == "__main__":
