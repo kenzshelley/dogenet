205de675677c978a1017f7cb9d02633f74617052
Damian Wieczorek <damianw@umich.edu>
2014-04-12 13:06:59 -0400

    i have no idea

--- linkreplace.py
+++ linkreplace.py
@@ -15,6 +15,7 @@ from bs4 import BeautifulSoup, NavigableString
 import random
 import sys
 import requests
+import string
 
 RICKROLL = "http://127.0.0.1:3000/rickroll"
 
@@ -66,11 +67,8 @@ def redditPhonyArticle(document):
     divider = soup.find("div", {"class" : "clearleft"})
 
     #will find the last article (should find the first though?)
-    for post in soup.find_all("div"):
-        postclasses = post.get("class")
-        if type(postclasses) is list:
-            if 'thing' in postclasses and 'link' in postclasses:
-                article = post        
+    articles = list(getArticles(soup))
+    article = random.choice(articles)     
 
     #ensures we insert at a even index
     insertionIndex = random.randint(1,20)
@@ -92,7 +90,15 @@ def redditPhonyArticle(document):
     siteTable.insert(insertionIndex + 1, newArticle)
     return soup.prettify()
 
+def getArticles(soup):
+    for post in soup.find_all("div"):
+        postclasses = post.get("class")
+        if type(postclasses) is list:
+            if 'thing' in postclasses and 'link' in postclasses:
+                yield post
+
 def multiPhonyArticles(document):
+    print "Multiphony!!!"
     for i in xrange(5):
         document = redditPhonyArticle(document)
     return document
@@ -105,28 +111,65 @@ def stackOverflowLinkReplacement(document):
     replacePercentLinks(tags, 0.5)
     return str(soup)
 
+class WTFString(str):
+    def __getitem__(self, key):
+        return WTFString('{%s}' % key)
+    def __getattribute__(self, key):
+        return WTFString('{%s}' % key)
+
+
+class WTFDict:
+    def __init__(self):
+        self.__dict = dict()
+
+    def __getitem__(self, name):
+        print "is this getting called"
+        return self.__dict.get(name, WTFString('{%s}' % name))
+
+    def __setitem__(self, key, value):
+        value = WTFString(value)
+        self.__dict[key] = value
+        return self.__dict[key]
+
+    def __missing__(self, name):
+        return WTFString('{%s}' % name)
+
 def stackOverflowPostReplacement(document):
     percent = 0.5
-    RESPONSES = ['I think <a href="%s">this post</a> really covers what you\'re looking for.' % RICKROLL,
-                 'Here, have a look at <a href="%s">this mega enlightening thread</a>.' % RICKROLL,
-                 'Forget all of that. <a href="%s">You could be making money every month by doing nothing!</a>' % RICKROLL,
-                 'More importantly, scientists have just discovered that Obama is <a href="%s">LITERALLY HITLER!</a>' % RICKROLL,
-                 '<a href="%s">Here\'s a tutorial</a> on how to do exactly what you want in Brainfuck!' % RICKROLL]
     soup = BeautifulSoup(document)
+    broll = soup.new_tag('a', href=RICKROLL)
+    RESPONSES = [("I think ", soup.new_tag('a', href=RICKROLL).append("this post"), "really covers what you're looking for."),
+                 ("Here, have a look at ", soup.new_tag('a', href=RICKROLL).append("this mega enlightening thread"), ""),
+                 ("Forget all of that. ", soup.new_tag('a', href=RICKROLL).append("You could be making money every month by doing nothing!"), "",),
+                 ("More importantly, scientists have just discovered that Obama is ", soup.new_tag('a', href=RICKROLL).append("LITERALLY HITLER!"), ""),
+                 ("", soup.new_tag('a', href=RICKROLL).append("Here's a tutorial"), " on how to do exactly what you want in Brainfuck!")]
+                 # 'I think <a href="%s">this post</a> really covers what you\'re looking for.' % RICKROLL,
+                 # 'Here, have a look at <a href="%s">this mega enlightening thread</a>.' % RICKROLL,
+                 # 'Forget all of that. <a href="%s">You could be making money every month by doing nothing!</a>' % RICKROLL,
+                 # 'More importantly, scientists have just discovered that Obama is <a href="%s">LITERALLY HITLER!</a>' % RICKROLL,
+                 # '<a href="%s">Here\'s a tutorial</a> on how to do exactly what you want in Brainfuck!' % RICKROLL]
     posts = soup.find_all("div", {'class': 'post-text'})
     comments = soup.find_all("span", {'class': 'comment-copy'})
     for post in random.sample(posts, int(percent*len(posts))):
-        post.string = BeautifulSoup("<p>%s</p>" % random.choice(RESPONSES)).encode(formatter=None)
+        resp = random.choice(RESPONSES)
+        p = soup.new_tag('p')
+        for val in resp:
+            p.append(val)
+        post.clear()
+        post.append(p)
     for comment in random.sample(comments, int(percent*len(comments))):
-        comment.string = BeautifulSoup(random.choice(RESPONSES)).encode(formatter=None)
+        resp = random.choice(RESPONSES)
+        comment.clear()
+        for val in resp:
+            comment.append(val)
     return str(soup)
 
 ## The dict of functions to do it
-HACKS = {'reddit.com': multiPhonyArticles,
-         'stackoverflow.com': stackOverflowPostReplacement}
+HACKS = {'www.reddit.com': multiPhonyArticles,
+         'stackoverflow.com': stackOverflowLinkReplacement}
 
 def anchors(document):
-    return anchorsToRickRoll(document, 0.5)
+    return anchorsToRickRoll(document, 0.2)
 
 if __name__ == "__main__":
     f = open(sys.argv[1], 'r')
@@ -136,4 +179,5 @@ if __name__ == "__main__":
 
 ## master routing function that all html goes through
 def hack(url, document):
-   return HACKS.get(url.split("/")[2], anchors)(document)
+    print "Da url: %s" % url.split("/")[2]
+    return HACKS.get(url.split("/")[2], anchors)(document)
