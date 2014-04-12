7063fb8731bebcc904f2ba39df918a5308629e90
Mackenzie Shelley <kenzshelley@gmail.com>
2014-04-12 01:00:18 -0400

    made js add new rows to leaderboard table

--- leaderboard.js
+++ leaderboard.js
@@ -15,6 +15,13 @@ $(document).ready(function(){
 			for(var i = 0; i < leaders.length; i++){
 				console.log(leaders[i].get("ip"));
 				console.log(leaders[i].get("score"));
+				//$('#leaderboard').append('<tr><td>leaders[i].get("ip")</td><td>leaders[i].get("score")</td></tr>')
+				var table = document.getElementById("leaderboard-table")
+				var row = table.insertRow(-1);
+				var cell1 = row.insertCell(0);
+				var cell2 = row.insertCell(1);
+				cell1.innerHTML = leaders[i].get("ip");
+				cell2.innerHTML = leaders[i].get("score");
 			}
 		}
 	});
